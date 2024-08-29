e618158c8227bb80208e0f195efb38c6725f5ddf
thomaselain <elainthomas0@gmail.com>
2024-08-29 18:15:23 +0200

    Adding Mineral class to generate different Perlin noises + New Units class (WIP)

--- ant_layer.js
+++ ant_layer.js
@@ -1,10 +1,25 @@
 class Mineral {
-    constructor(rarity, tint) {   
+    constructor(rarity, type) {   
         this.rarity = rarity;
-        this.tint = tint;
+        this.type = type;
     }
 }
 
+class Unit {
+    constructor(x, y, race, type) {
+        this.x = x;
+        this.y = y;
+        this.race = race;
+        this.type = type;
+    }
+}
+
+//test 
+let Thomas = new Unit(320, 240, "Human", "Warrior");
+
+
+let storedMap = [];
+let air;
 let iron;
 let gold;
 let water;
@@ -15,17 +30,29 @@ let d;
 function setup() {
     createCanvas(640, 480);
     
+    // ????? (NE PAS TOUCHER)
     d = pixelDensity();
-    iron = new Mineral(166, color(220, 210, 180));
-    gold = new Mineral(151, color(220, 180, 30));
-    water = new Mineral(171, color(30, 50, 210));
-    rocks = new Mineral(186, color(98, 40, 30));
-    
-    background(20, 16, 30);
-    createRessourceMap(iron);
-    createRessourceMap(gold);
+    for (let i = 0; i < width; i++) {
+      storedMap[i] = []; // create nested array
+      for (let j = 0; j < height; j++) {
+        storedMap[i][j] = 0;
+      }
+    }
+  
+    // Listing mineral types and their properties
+//    air = new Mineral(50, 0);
+    iron = new Mineral(50, 1);
+    gold = new Mineral(25, 2);
+    water = new Mineral(140, 3);
+    rocks = new Mineral(186, 4);
+        
     createRessourceMap(water);
     createRessourceMap(rocks);
+    createRessourceMap(iron);
+    createRessourceMap(gold);
+
+    drawMap();
+    
 }
 
 function setPixel(c, x, y, light) {
@@ -38,53 +65,96 @@ function setPixel(c, x, y, light) {
       pixels[index + 3] = alpha(c) * light / 255;
     }
   }
-    //pixels[y * width + x + 0] = red(c);
-    //pixels[y * width + x + 1] = green(c);
-    //pixels[y * width + x + 2] = blue(c);
-    //pixels[y * width + x + 3] = alpha(c);
 }
 
+function storePixel(cellType, x, y)
+{
+    storedMap[x][y] = cellType;
+}
+
+// Store minerals in storedMap
 function createRessourceMap(mineral) {
-    // Set the noise level and scale.
-    noiseSeed(mineral.rarity);
+    noiseSeed();
     noiseDetail(2, 1.1);
-    loadPixels();
     let noiseLevel = 255;
-    let noiseScale = 0.02;
-    // Iterate from top to bottom.
+    let noiseScale = 0.035;
     for (let y = 0; y < height; y += 1) {
-        // Iterate from left to right.
         for (let x = 0; x < width; x += 1) {
-            // Scale the input coordinates.
-            let nx = noiseScale * x; (x - height / 2) * height;
-            let ny = noiseScale * y; (y - width / 2) * width;
+            let nx = noiseScale * x; //* (x - height / 2) * height;
+            let ny = noiseScale * y; //* (y - width / 2) * width;
 
             let c = noiseLevel * noise(nx + (1 * 0.000001 + 10000) * (mineral.rarity - 100 * 100), ny + (1 * 0.000001 + 1000) * (100*mineral.rarity - 100));
 
-            // Draw the point.
-            if (c + 3 > mineral.rarity && c - 3 < mineral.rarity) 
-            {
-                setPixel(color(0), x, y, 255);
-                //stroke(mineral.tint);
-                //point(x, y);
-            }
-            else if (c >= mineral.rarity) 
-            {
-                setPixel(mineral.tint, x, y, 255);
-                //stroke(mineral.tint);
-                //point(x, y);
-            }
-
+            if (c <= mineral.rarity) 
+                storePixel(mineral.type, x, y);
         }
     }
+}
+
+function drawMap()
+{
+    loadPixels();
+
+    // Loop in storedMap
+    for (let i = 0; i < storedMap.length; i += 1)
+    {
+        for (let j = 0; j < storedMap[i].length; j += 1)
+        {
+            switch (storedMap[i][j])
+            {
+                case 1: 
+                  c = color(220, 210, 180);
+                  break;
+                case 2:
+                  c= color(220, 180, 30);
+                  break;
+                case 3:
+                  c = color(30, 50, 210);
+                  break;
+                case 4:
+                  c = color(98, 40, 30);
+                  break;
+                //case 0:
+                default:
+                    c = color(0, 0, 0, 255);
+            }
+        setPixel(c, i, j, 255);
+        }
+    }
+
     updatePixels();
 }
 
+// to remove
+function keyPressed()
+{
+     if (keyIsDown(UP_ARROW) === true)
+        Thomas.y--;
+     if (keyCode === LEFT_ARROW)
+        Thomas.x--;
+     if (keyCode === DOWN_ARROW)
+        Thomas.y++;
+     if (keyCode === RIGHT_ARROW)
+        Thomas.x++;
+    console.log(Thomas.x, Thomas.y);
+}
+
+function drawUnits()
+{
+     loadPixels();
+
+     setPixel(color(35, 206, 235), Thomas.x, Thomas.y+1, 255);
+     setPixel(color(35, 206, 235), Thomas.x+1, Thomas.y, 255);
+     setPixel(color(255,255, 255), Thomas.x, Thomas.y, 255);
+     setPixel(color(35, 206, 235), Thomas.x, Thomas.y-1, 255);
+     setPixel(color(35, 206, 235), Thomas.x-1, Thomas.y, 255);
+     
+     updatePixels();
+}
+
 function draw() {
-    
-    //background(40, 32, 59);
-    //createRessourceMap(iron);
-    //createRessourceMap(gold);
-    //createRessourceMap(water);
-    //createRessourceMap(rocks);
+    background(20, 16, 28);
+    drawMap();
+//    drawBuildings();
+    drawUnits();
 }
